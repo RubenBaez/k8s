c6cf3e44cdee3cbef016c33c8a08ba869f13b892
rubenbaez <rubenbaez1995@gmail.com>
2018-06-08 11:18:20 -0500

    add get requests

---
+++ triggers/flask/get/getTrigger.py
@@ -0,0 +1,12 @@
+import os 
+import requests
+
+result = requests.get("http://192.168.99.100:30050")
+
+print(result.status_code)
+
+if(result.status_code==200):
+	print("Accedido :)")
+else:
+	print("Algo va mal..")
+
--- triggers/getMicroservice.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-alias http='docker run -it --rm mcampbell/httpie'
-while true; do http http://192.168.99.100:32218/; echo "esperando..."; sleep 1; done
