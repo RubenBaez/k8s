cfc7e77533da1d05e9d301557b2c170645182bf9
rubenbaez <rubenbaez1995@gmail.com>
2018-06-08 15:48:33 -0500

    add requests tornado and fix encoding

--- triggers/tornado/delete/deleteTrigger.py
+++ triggers/tornado/delete/deleteTrigger.py
@@ -1,4 +1,3 @@
-#encoding: utf-8
 import os
 import requests
 from pymongo import MongoClient
@@ -23,12 +22,12 @@ def consultas():
 def requestMicroservice():
 	items = consultas()
 	for i in items:
-		result = requests.get("http://192.168.99.100:30050/delete/"+i) 
+		result = requests.get("http://192.168.99.100:31216/deletetornado/"+i) 
 		print(result.status_code)
 		if(result.status_code==200):
 			print("Borrado :)")
 			time.sleep(60)
 		else:
 			print("Algo va mal..")
-			time.sleep(60)
+			time.sleep(1)
 requestMicroservice()
