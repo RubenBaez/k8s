14817d94e26a9ef5b2acee8f14063d161b795c4e
rubenbaez <rubenbaez1995@gmail.com>
2018-06-11 17:04:23 -0500

    change flask requests

--- triggers/flask/delete/deleteMicroservice.sh
+++ triggers/flask/delete/deleteMicroservice.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-alias http='docker run -it --rm mcampbell/httpie'
 while true; do python deleteTrigger.py; echo "esperando..."; done
--- triggers/flask/update/updateMicroservice.sh
+++ triggers/flask/update/updateMicroservice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 alias http='docker run -it --rm mcampbell/httpie'
-while true; do python updateTrigger.py; echo "esperando..."; sleep 1; done
+while true; do python updateTrigger.py; echo "esperando..."; sleep 3001; done
