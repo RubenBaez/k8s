71cd9ec07c6c326933ec2fbe9c0502fb0b27075b
rubenbaez <rubenbaez1995@gmail.com>
2018-06-27 17:53:34 -0500

    add new unit test to post

--- Microservices-to-K8s-flask/post/test_app.py
+++ Microservices-to-K8s-flask/post/test_app.py
@@ -11,6 +11,28 @@ class TestConference(unittest.TestCase):
         response = self.test_app.get("http://192.168.99.100:30050/post")
         self.assertEquals(response.status, "200 OK")
 
+    def test_sending_form(self):
+        # Use Flask's test client for our test.
+        self.test_app = a.test_client()
+
+        response = self.test_app.post("http://192.168.99.100:30050/post", data={"username":"rubenbaez", 
+        "event_source":"server", 
+        "name":"rubenbaez", 
+        "accept_languaje":"español", 
+        "time":"2016-12-15T17:28:39.146372+00:00", 
+        "agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4)", 
+        "page":"None", 
+        "host":"soer.utpl.edu.ec", 
+        "session":"287", 
+        "referer":"http://soer.utpl.edu.ec/home/", 
+        "user_id":258, 
+        "org_id":"1241", 
+        "course_id":"54123", 
+        "path":"/course/course-v1:UTPL+AIRPOLLUTION+2017_ENE", 
+        "ip":"172.18.7.142", 
+        "mode":"CR-452", 
+        "event_type":"/course/course-v1:UTPL+AIRPOLLUTION+2017_ENE" })
+
     def test_db(self):
         result = app.con()
         result = str(result)
